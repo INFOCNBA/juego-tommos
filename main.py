3166c67fc57821a7a5cbadbe1955ea0db29cea70
Informática CNBA <105891917+INFOCNBA@users.noreply.github.com>
2022-10-26 19:36:49 -0300

    Tommos: Los cambios que entendí de la v1.4 son: - niveles: a los 100,200,300 puntos cambia de nivel- nivel0: una """bagotala"""  (como en juego v1.3)- nivel1: dos bagotalas  (como en juevo v1.4, el que yo te mandé)- nivel2: tres bagotalas  (dos horizontales, una vertical) - nivel3: tres bagotalas  verticales- a los 400 el juego se cuelga - si sos Profe, y tenés entre 190*5 y 191*5 puntos , perdés por canchero... 

--- main.py
+++ main.py
@@ -6,35 +6,149 @@ def on_button_pressed_b():
     sprite.move(1)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+bala3: game.LedSprite = None
+lado = 0
 bala2: game.LedSprite = None
 bala: game.LedSprite = None
-fila_de_inicio = 0
+arriba_o_derecha = 0
+puntos = 0
+p_real = 0
 sprite: game.LedSprite = None
 vida = 3
-puntos = 0
 sprite = game.create_sprite(1, 3)
 
 def on_forever():
-    global fila_de_inicio, bala, bala2, vida, puntos
+    global p_real, arriba_o_derecha, bala, vida, puntos, bala2, lado, bala3
     basic.pause(3000)
     while game.is_running():
-        fila_de_inicio = randint(0, 4)
-        bala = game.create_sprite(1, fila_de_inicio)
-        bala2 = game.create_sprite(0, (fila_de_inicio + 3) % 5)
-        for index in range(5):
-            basic.pause(500)
-            bala.move(1)
-            bala2.move(1)
-            if sprite.is_touching(bala) or sprite.is_touching(bala2):
-                vida += -1
-                music.play_melody("F - B - - - - - ", 500)
+        p_real = Math.idiv(puntos, 5)
+        if p_real < 20:
+            arriba_o_derecha = randint(0, 4)
+            bala = game.create_sprite(0, arriba_o_derecha)
+            for index in range(5):
+                basic.pause(500)
+                bala.move(1)
+                if sprite.is_touching(bala):
+                    vida += -1
+                    music.play_melody("F - B - - - - - ", 500)
+                else:
+                    puntos += 1
+                if vida == 0:
+                    music.play_melody("D F E G F A G B ", 120)
+                    bala.delete()
+                    sprite.delete()
+                    for index2 in range(3):
+                        basic.show_number(p_real)
+                    game.game_over()
+            bala.delete()
+        elif p_real >= 20 and p_real < 40:
+            arriba_o_derecha = randint(0, 4)
+            bala = game.create_sprite(1, arriba_o_derecha)
+            bala2 = game.create_sprite(0, (arriba_o_derecha + 3) % 5)
+            for index3 in range(5):
+                basic.pause(500)
+                bala.move(1)
+                bala2.move(1)
+                if sprite.is_touching(bala) or sprite.is_touching(bala2):
+                    vida += -1
+                    music.play_melody("F - B - - - - - ", 500)
+                else:
+                    puntos += 1
+                if vida == 0:
+                    music.play_melody("D F E G F A G B ", 120)
+                    bala.delete()
+                    sprite.delete()
+                    bala2.delete()
+                    for index4 in range(3):
+                        basic.show_number(p_real)
+                    game.game_over()
+            bala.delete()
+            bala2.delete()
+        elif p_real >= 40 and p_real < 60:
+            lado = randint(1, 2)
+            arriba_o_derecha = randint(0, 4)
+            if lado == 1:
+                bala = game.create_sprite(0, arriba_o_derecha)
             else:
-                puntos += 1
-            if vida == 0:
-                music.play_melody("D F E G F A G B ", 120)
-                for index2 in range(3):
-                    basic.show_number(Math.idiv(puntos, 5))
-                game.game_over()
-        bala.delete()
-        bala2.delete()
+                bala = game.create_sprite(arriba_o_derecha, 0)
+                bala.turn(Direction.RIGHT, 90)
+            lado = randint(1, 2)
+            arriba_o_derecha = randint(0, 4)
+            if lado == 1:
+                bala2 = game.create_sprite(0, arriba_o_derecha)
+            else:
+                bala2 = game.create_sprite(arriba_o_derecha, 0)
+                bala2.turn(Direction.RIGHT, 90)
+            for index5 in range(5):
+                basic.pause(500)
+                bala.move(1)
+                bala2.move(1)
+                if sprite.is_touching(bala) or sprite.is_touching(bala2):
+                    vida += -1
+                    music.play_melody("F - B - - - - - ", 500)
+                else:
+                    puntos += 1
+                if vida == 0:
+                    music.play_melody("D F E G F A G B ", 120)
+                    sprite.delete()
+                    bala.delete()
+                    bala2.delete()
+                    for index6 in range(3):
+                        basic.show_number(p_real)
+                    game.game_over()
+            bala.delete()
+            bala2.delete()
+        elif p_real >= 60 and p_real < 80:
+            lado = randint(1, 2)
+            arriba_o_derecha = randint(0, 4)
+            if lado == 1:
+                bala = game.create_sprite(0, arriba_o_derecha)
+            else:
+                bala = game.create_sprite(arriba_o_derecha, 0)
+                bala.turn(Direction.RIGHT, 90)
+            lado = randint(1, 2)
+            arriba_o_derecha = randint(0, 4)
+            if lado == 1:
+                bala2 = game.create_sprite(0, arriba_o_derecha)
+            else:
+                bala2 = game.create_sprite(arriba_o_derecha, 0)
+                bala2.turn(Direction.RIGHT, 90)
+            lado = randint(1, 2)
+            arriba_o_derecha = randint(0, 4)
+            if lado == 1:
+                bala3 = game.create_sprite(0, arriba_o_derecha)
+            else:
+                bala3 = game.create_sprite(arriba_o_derecha, 0)
+                bala3.turn(Direction.RIGHT, 90)
+            for index7 in range(5):
+                basic.pause(500)
+                bala.move(1)
+                bala2.move(1)
+                bala3.move(1)
+                if sprite.is_touching(bala) or (sprite.is_touching(bala2) or sprite.is_touching(bala3)):
+                    vida += -1
+                    music.play_melody("F - B - - - - - ", 500)
+                else:
+                    puntos += 1
+                if vida == 0:
+                    music.play_melody("D F E G F A G B ", 120)
+                    sprite.delete()
+                    bala2.delete()
+                    bala.delete()
+                    bala3.delete()
+                    for index8 in range(3):
+                        basic.show_number(p_real)
+                    game.game_over()
+            bala.delete()
+            bala2.delete()
+            bala3.delete()
+        elif p_real == 190:
+            music.play_melody("D F E G F A G B ", 120)
+            sprite.delete()
+            bala2.delete()
+            bala.delete()
+            bala3.delete()
+            for index9 in range(3):
+                basic.show_number(p_real)
+            game.game_over()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -4,38 +4,171 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     sprite.move(1)
 })
-let puntos = 0
+let bala3: game.LedSprite = null
+let lado = 0
 let bala2: game.LedSprite = null
 let bala: game.LedSprite = null
-let fila_de_inicio = 0
+let arriba_o_derecha = 0
+let puntos = 0
+let p_real = 0
 let sprite: game.LedSprite = null
 let vida = 3
 sprite = game.createSprite(1, 3)
 basic.forever(function () {
     basic.pause(3000)
     while (game.isRunning()) {
-        fila_de_inicio = randint(0, 4)
-        bala = game.createSprite(1, fila_de_inicio)
-        bala2 = game.createSprite(0, (fila_de_inicio + 3) % 5)
-        for (let index = 0; index < 5; index++) {
-            basic.pause(500)
-            bala.move(1)
-            bala2.move(1)
-            if (sprite.isTouching(bala) || sprite.isTouching(bala2)) {
-                vida += -1
-                music.playMelody("F - B - - - - - ", 500)
-            } else {
-                puntos += 1
-            }
-            if (vida == 0) {
-                music.playMelody("D F E G F A G B ", 120)
-                for (let index = 0; index < 3; index++) {
-                    basic.showNumber(Math.idiv(puntos, 5))
+        p_real = Math.idiv(puntos, 5)
+        if (p_real < 20) {
+            arriba_o_derecha = randint(0, 4)
+            bala = game.createSprite(0, arriba_o_derecha)
+            for (let index = 0; index < 5; index++) {
+                basic.pause(500)
+                bala.move(1)
+                if (sprite.isTouching(bala)) {
+                    vida += -1
+                    music.playMelody("F - B - - - - - ", 500)
+                } else {
+                    puntos += 1
                 }
-                game.gameOver()
+                if (vida == 0) {
+                    music.playMelody("D F E G F A G B ", 120)
+                    bala.delete()
+                    sprite.delete()
+                    for (let index = 0; index < 3; index++) {
+                        basic.showNumber(p_real)
+                    }
+                    game.gameOver()
+                }
             }
+            bala.delete()
+        } else if (p_real >= 20 && p_real < 40) {
+            arriba_o_derecha = randint(0, 4)
+            bala = game.createSprite(1, arriba_o_derecha)
+            bala2 = game.createSprite(0, (arriba_o_derecha + 3) % 5)
+            for (let index = 0; index < 5; index++) {
+                basic.pause(500)
+                bala.move(1)
+                bala2.move(1)
+                if (sprite.isTouching(bala) || sprite.isTouching(bala2)) {
+                    vida += -1
+                    music.playMelody("F - B - - - - - ", 500)
+                } else {
+                    puntos += 1
+                }
+                if (vida == 0) {
+                    music.playMelody("D F E G F A G B ", 120)
+                    bala.delete()
+                    sprite.delete()
+                    bala2.delete()
+                    for (let index = 0; index < 3; index++) {
+                        basic.showNumber(p_real)
+                    }
+                    game.gameOver()
+                }
+            }
+            bala.delete()
+            bala2.delete()
+        } else if (p_real >= 40 && p_real < 60) {
+            lado = randint(1, 2)
+            arriba_o_derecha = randint(0, 4)
+            if (lado == 1) {
+                bala = game.createSprite(0, arriba_o_derecha)
+            } else {
+                bala = game.createSprite(arriba_o_derecha, 0)
+                bala.turn(Direction.Right, 90)
+            }
+            lado = randint(1, 2)
+            arriba_o_derecha = randint(0, 4)
+            if (lado == 1) {
+                bala2 = game.createSprite(0, arriba_o_derecha)
+            } else {
+                bala2 = game.createSprite(arriba_o_derecha, 0)
+                bala2.turn(Direction.Right, 90)
+            }
+            for (let index = 0; index < 5; index++) {
+                basic.pause(500)
+                bala.move(1)
+                bala2.move(1)
+                if (sprite.isTouching(bala) || sprite.isTouching(bala2)) {
+                    vida += -1
+                    music.playMelody("F - B - - - - - ", 500)
+                } else {
+                    puntos += 1
+                }
+                if (vida == 0) {
+                    music.playMelody("D F E G F A G B ", 120)
+                    sprite.delete()
+                    bala.delete()
+                    bala2.delete()
+                    for (let index = 0; index < 3; index++) {
+                        basic.showNumber(p_real)
+                    }
+                    game.gameOver()
+                }
+            }
+            bala.delete()
+            bala2.delete()
+        } else if (p_real >= 60 && p_real < 80) {
+            lado = randint(1, 2)
+            arriba_o_derecha = randint(0, 4)
+            if (lado == 1) {
+                bala = game.createSprite(0, arriba_o_derecha)
+            } else {
+                bala = game.createSprite(arriba_o_derecha, 0)
+                bala.turn(Direction.Right, 90)
+            }
+            lado = randint(1, 2)
+            arriba_o_derecha = randint(0, 4)
+            if (lado == 1) {
+                bala2 = game.createSprite(0, arriba_o_derecha)
+            } else {
+                bala2 = game.createSprite(arriba_o_derecha, 0)
+                bala2.turn(Direction.Right, 90)
+            }
+            lado = randint(1, 2)
+            arriba_o_derecha = randint(0, 4)
+            if (lado == 1) {
+                bala3 = game.createSprite(0, arriba_o_derecha)
+            } else {
+                bala3 = game.createSprite(arriba_o_derecha, 0)
+                bala3.turn(Direction.Right, 90)
+            }
+            for (let index = 0; index < 5; index++) {
+                basic.pause(500)
+                bala.move(1)
+                bala2.move(1)
+                bala3.move(1)
+                if (sprite.isTouching(bala) || (sprite.isTouching(bala2) || sprite.isTouching(bala3))) {
+                    vida += -1
+                    music.playMelody("F - B - - - - - ", 500)
+                } else {
+                    puntos += 1
+                }
+                if (vida == 0) {
+                    music.playMelody("D F E G F A G B ", 120)
+                    sprite.delete()
+                    bala2.delete()
+                    bala.delete()
+                    bala3.delete()
+                    for (let index = 0; index < 3; index++) {
+                        basic.showNumber(p_real)
+                    }
+                    game.gameOver()
+                }
+            }
+            bala.delete()
+            bala2.delete()
+            bala3.delete()
+        } else if (p_real == 190) {
+            music.playMelody("D F E G F A G B ", 120)
+            sprite.delete()
+            bala2.delete()
+            bala.delete()
+            bala3.delete()
+            for (let index = 0; index < 3; index++) {
+                basic.showNumber(p_real)
+            }
+            game.gameOver()
         }
-        bala.delete()
-        bala2.delete()
     }
 })
